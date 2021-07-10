319b56d0af08754e7b12180cd1c2a7885faf110a
Douglas Ferreira <dodofp.df@gmail.com>
2021-07-10 17:55:14 -0300

    Aplicando css para responsividade

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -26,14 +26,14 @@ function Projects() {
           <h2>Projects</h2>
           <img src={line} alt="roots"/>
         </header>
-        {window.matchMedia("(max-width: 414px)").matches ?
+        {window.matchMedia("(max-width: 850px)").matches ?
           <div className={Styles.projectContainer}> 
             <ProjectCard data={data[index]}/>
           </div>
         : <div className={Styles.projectContainer}>
           {data.map((project) => <ProjectCard data={project} key={project.name} />)}
         </div>}
-        {window.matchMedia("(max-width: 414px)").matches ?
+        {window.matchMedia("(max-width: 850px)").matches ?
         <div className={Styles.navigate}>
           <NavigateBeforeIcon onClick={handleBefore} color="primary" style={{ fontSize: 60}} />
           <NavigateNextIcon onClick={handleNext} color="primary" style={{ fontSize: 60 }} />
