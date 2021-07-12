dbbb5620c7e92c54c3dd923457b9ff758d2f9657
Douglas Ferreira <dodofp.df@gmail.com>
2021-07-12 11:18:30 -0300

    Primeiro dp

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import Provider from './context/Provider';
 
 ReactDOM.render(
     <React.StrictMode>
     <Provider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
     </React.StrictMode>,
   document.getElementById('root')
